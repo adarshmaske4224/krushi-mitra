ece2c4f6ae6d58511cb461b1382fa1afded6918c
adarsh <adarshmaske2005@gmail.com>
2026-03-03 18:45:20 +0530

    firstcommit

---
+++ src/main/java/com/example/KrushiMitra/KrushiMitraApplication.java
@@ -0,0 +1,13 @@
+package com.example.KrushiMitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrushiMitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrushiMitraApplication.class, args);
+	}
+
+}
